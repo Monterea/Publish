1ede42c156cc89f2d73527255a0e59d85f41ad0d
Michaela Stuchlíková <101055513+Monterea@users.noreply.github.com>
2022-05-12 06:15:16 +0200

    Add files via upload

---
+++ Inventura/Inventura/MainActivity.cs
@@ -0,0 +1,87 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Inventura{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity{
+        int sum = 0;
+        int count = 0;
+        List<int> counts=new List<int>();
+        Button buttonIncrement;
+        Button buttonDecrement;
+        TextView textCount;
+        Button buttonIncrementMore;
+        Button buttonDecrementLast;
+        Button buttonReset;
+        Button buttonAddObject;
+        Button buttonNextObject;
+        Button buttonViewChanges;
+        Button buttonViewObjectsSum;
+
+        protected override void OnCreate(Bundle savedInstanceState){
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            buttonIncrement = FindViewById<Button>(Resource.Id.buttonIncrement);
+            buttonDecrement = FindViewById<Button>(Resource.Id.buttonDecrement);
+            textCount = FindViewById<TextView>(Resource.Id.textViewCount);
+            buttonReset = FindViewById<Button>(Resource.Id.buttonReset);
+
+            buttonIncrementMore = FindViewById<Button>(Resource.Id.buttonIncrementMore);
+            buttonDecrementLast = FindViewById<Button>(Resource.Id.buttonDecrementLast);
+            
+            buttonAddObject = FindViewById<Button>(Resource.Id.buttonAddObject);
+           buttonNextObject = FindViewById<Button>(Resource.Id.buttonNextObject);
+            buttonViewChanges = FindViewById<Button>(Resource.Id.buttonViewChanges);
+            buttonViewObjectsSum = FindViewById<Button>(Resource.Id.buttonViewObjectsSum);
+
+
+            buttonIncrement.Click += ButtonIncrement_Click;
+            buttonDecrement.Click += ButtonDecrement_Click;
+            buttonReset.Click += ButtonReset_Click;
+            buttonViewChanges.Click += ButtonViewChanges_Click;
+            buttonIncrementMore.Click += ButtonIncrementMore_Click;
+        }
+
+        private void ButtonIncrementMore_Click(object sender, EventArgs e) {
+            sum = sum + count;
+            ShowTheSum();
+        }
+
+        private void ButtonViewChanges_Click(object sender, EventArgs e) {
+            foreach(int sum in counts) {
+                Console.WriteLine(sum);
+            }
+        }
+
+        private void ButtonReset_Click(object sender, EventArgs e) {
+            sum=0;
+            ShowTheSum();
+        }
+
+        private void ButtonDecrement_Click(object sender, EventArgs e) {
+            sum--;
+            ShowTheSum();
+        }
+
+        private void ShowTheSum() {
+            counts.Add(sum);
+            textCount.Text = sum.ToString();
+        }
+
+        private void ButtonIncrement_Click(object sender, EventArgs e) {
+            sum++;
+            ShowTheSum();
+        }
+
+
+    }
+}
---
+++ OdpocitavadloDnu/OdpocitavadloDnu/MainActivity.cs
@@ -0,0 +1,101 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace OdpocitavadloDnu
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity{
+        TextView textViewDays;
+        RadioButton rbHalloween;
+        RadioButton rbKidsDay;
+        RadioButton rbChristmas;
+        TextInputEditText textInputCustomDate;
+        Button buttonCountDays;
+//datové složky pro výpočet:
+DateTime today = DateTime.Today;
+            DateTime endDate;
+            TimeSpan daysToEvent;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //udělaná metoda pro inicializaci prvků uživatelského rozhraní:
+            SetupReferences();
+            //udělaná metoda pro vložení událostí
+            SubscribeEvenHandlers();
+
+        }
+        private void SetupReferences() {
+            textViewDays = FindViewById<TextView>(Resource.Id.textViewDays);
+            rbHalloween = FindViewById<RadioButton>(Resource.Id.radioButtonHalloween);
+            rbKidsDay = FindViewById<RadioButton>(Resource.Id.radioButtonKidsDay);
+            rbChristmas = FindViewById<RadioButton>(Resource.Id.radioButtonChristmas);
+            textInputCustomDate = FindViewById<TextInputEditText>(Resource.Id.textInputCustomDate);
+            buttonCountDays=FindViewById<Button>(Resource.Id.buttonCountDays)
+        }
+        private void SubscribeEvenHandlers() {
+            rbHalloween.CheckedChange += RbHalloween_CheckedChange;
+            rbKidsDay.CheckedChange += rbKidsDay_CheckedChange;
+            rbChristmas.CheckedChange += RbChristmas_CheckedChange;
+            buttonCountDays.Click += ButtonCountDays_Click;
+        }
+        private void RbHalloween_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e) {
+            if (rbHalloween.Checked) {
+                /*   
+                    DateTime today = DateTime.Today;
+                    DateTime endDate = new DateTime(today.Year, 10, 31);
+                    //pozor!!  když se dělá výpočet s daty je výsledek typu TimeSpan
+                    TimeSpan daysToEvent = endDate - today;
+                    textViewDays.Text = daysToEvent.ToString();
+
+                předěláme na metodu:
+                */
+                CountEndDate(31, 10, today.Year);
+                CountTimeSpan();
+                ViewTheCountOfDays("Halloween");
+            }
+        }
+        private void RbKidsDay_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e) {
+            if (rbKidsDay.Checked) {
+                CountEndDate(1, 6, today.Year);
+                CountTimeSpan();
+                ViewTheCountOfDays("Summer");
+            }
+        }
+
+        private void RbChristmas_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e) {
+            if (rbChristmas.Checked) {
+                CountEndDate(24, 12, today.Year);
+                CountTimeSpan();
+                ViewTheCountOfDays("Christmas");
+            }
+        }
+
+
+        //předělání společných příkazů na metody
+        private void CountEndDate(int day, int month, int year) {
+            endDate = new DateTime(year, month, day);
+            //zjišťujeme jestli není dnešní aktuální den vyšší než den, který hledáme
+            if (today.DayOfYear > endDate.DayOfYear) { //pokud ano tak se bude odpočítávat hledaný den z dalšího roku                                                   
+                endDate = endDate.AddYears(1);
+            }
+
+            private void CountTimeSpan() {
+                TimeSpan daysToEvent = endDate - today;
+            }
+            private void ViewTheCountOfDays(string event) {
+                textViewDays.Text = event;
+        }
+
+
+
+
+    }
+}
